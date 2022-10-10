bf4f0ee565c7e1257f0f6ed1743a532f3ef674e7
Logesh kumar <phoenix.logu@gmail.com>
2022-10-10 17:39:31 +0530

    use name function in background script

--- src/background.ts
+++ src/background.ts
@@ -1,18 +1,22 @@
 import { getSystemInfo } from './utils'
 
-getSystemInfo(({ cpu: { modelName, usage } }) => {  
-  const idle =
-    usage.reduce(
-      (prevValue, currValue) => prevValue + currValue.idle / currValue.total,
-      0
-    ) / usage.length // usage.length is the number of processors
+function init() {
+  getSystemInfo(({ cpu: { modelName, usage } }) => {
+    const idle =
+      usage.reduce(
+        (prevValue, currValue) => prevValue + currValue.idle / currValue.total,
+        0
+      ) / usage.length // usage.length is the number of processors
 
-  const cpuUsage = Math.round((1 - idle) * 100)
+    const cpuUsage = Math.round((1 - idle) * 100)
 
-  // console.log(`${modelName} - ${cpuUsage}%`)
+    // console.log(`${modelName} - ${cpuUsage}%`)
 
-  // send to content script to all tabs
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs: any) => {
-    chrome.tabs.sendMessage(tabs[0].id, { cpuUsage })
+    // send to content script to save to indexedDB
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs: any) => {
+      chrome.tabs.sendMessage(tabs[0]?.id, { cpuUsage })
+    })
   })
-})
+}
+
+init()
